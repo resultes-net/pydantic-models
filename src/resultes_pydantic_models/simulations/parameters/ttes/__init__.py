503a947d7322fbe45de8a4cad614fba21f390fc8
Damian Birchler <damian.birchler@ost.ch>
2025-05-22 10:45:33 +0200

    Parameters `type` as descriminator.

--- src/resultes_pydantic_models/simulations/parameters/ttes/__init__.py
+++ src/resultes_pydantic_models/simulations/parameters/ttes/__init__.py
@@ -1,3 +1,5 @@
+import typing as _tp
+
 import pydantic as _pc
 
 from .parameters import thermal_energy_storage as _tes
@@ -6,6 +8,7 @@ from ..common import demand as _demand
 
 
 class TtesParameters(_pc.BaseModel):
+    type: _tp.Literal["ttes"]
     demand: _demand.Demand
     collector_field: _cf.CollectorField
     storage: _tes.TtesStorage
