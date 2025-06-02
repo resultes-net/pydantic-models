9adcbd8c6777de739a0c34b04bd58efd63408c8c
Damian Birchler <damian@damianbirchler.net>
2025-06-02 22:47:15 +0200

    Make version optional.

--- src/resultes_pydantic_models/pytrnsys.py
+++ src/resultes_pydantic_models/pytrnsys.py
@@ -6,7 +6,7 @@ import pydantic as _pyd
 class ObjectStoragePath(_pyd.BaseModel):
     container: str
     path: str
-    version: str | None
+    version: str | None = None
 
 class RunnerJob(_pyd.BaseModel):
     object_storage_path: ObjectStoragePath
