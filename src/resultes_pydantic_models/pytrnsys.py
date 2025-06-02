f0aaca22c1ee0c235d76006b2b8bcdca21ab0445
Damian Birchler <damian@damianbirchler.net>
2025-06-03 00:16:57 +0200

    Add more needed fields to runner job.

--- src/resultes_pydantic_models/pytrnsys.py
+++ src/resultes_pydantic_models/pytrnsys.py
@@ -1,13 +1,23 @@
-import pathlib as _pl
-
 import pydantic as _pyd
 
 
-class ObjectStoragePath(_pyd.BaseModel):
+class ObjectStorageZipPath(_pyd.BaseModel):
     container: str
     path: str
     version: str | None = None
 
+    @_pyd.field_validator("path", mode="after")
+    @classmethod
+    def _validate_path(cls, value: str) -> str:
+        if not value.endswith(".zip"):
+            raise ValueError("Path must point to .zip file.")
+
+        return value
+
+
 class RunnerJob(_pyd.BaseModel):
-    object_storage_path: ObjectStoragePath
+    id: str
+    object_storage_path: ObjectStorageZipPath
     script_to_run: str
+    working_dir: str | None = None
+    results_dir_to_list: str | None = None
