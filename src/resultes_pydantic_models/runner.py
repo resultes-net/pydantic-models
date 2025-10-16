d1a848da1fbb4a0801a7b6a9542c42c5b1ca0fbc
Damian Birchler <damian.birchler@ost.ch>
2025-10-16 12:22:46 +0200

    Allow running multiple commands in a row and upload multiple result files.

--- src/resultes_pydantic_models/runner.py
+++ src/resultes_pydantic_models/runner.py
@@ -1,4 +1,7 @@
+import abc as _abc
 import collections.abc as _cabc
+import pathlib as _pl
+import typing as _tp
 
 import pydantic as _pyd
 import resultes_pydantic_models.common as _pcom
@@ -9,25 +12,68 @@ class RunnerOptions(_pyd.BaseModel):
     shall_remove_completed_jobs: bool
 
 
-class ObjectStorageZipPath(_pyd.BaseModel):
+class ObjectStorageFilePathBase(_pyd.BaseModel, _abc.ABC):
     container: str
     path: str
-    version: str | None = None
 
+
+class ObjectStorageZipFilePathBase(ObjectStorageFilePathBase, _abc.ABC):
     @_pyd.field_validator("path", mode="after")
     @classmethod
     def _validate_path(cls, value: str) -> str:
         if not value.endswith(".zip"):
-            raise ValueError("Path must point to .zip file.")
+            raise ValueError("Path must point to .zip file.", value)
 
         return value
 
 
-class RunnerJob(_pyd.BaseModel):
-    id: str
-    object_storage_path: ObjectStorageZipPath
+class ObjectStorageInputZipFilePath(ObjectStorageZipFilePathBase):
+    version: str | None = None
+
+
+class ObjectStorageOutputFilePath(ObjectStorageFilePathBase):
+    pass
+
+
+class ObjectStorageOutputZipFilePath(ObjectStorageZipFilePathBase):
+    pass
+
+
+class SingleFileResult(_pyd.BaseModel):
+    discriminator = "single"
+    file_path: _pl.PureWindowsPath
+    object_storage_output_file_path: ObjectStorageOutputFilePath
+
+    @_pyd.field_validator("file_path", mode="after")
+    @classmethod
+    def _validate_file_path(cls, value: _pl.PureWindowsPath) -> _pl.PureWindowsPath:
+        if value.is_absolute():
+            raise ValueError("File path must not be absolute.", value)
+
+        return value
+
+
+class MultipleFilesResult(_pyd.BaseModel):
+    discriminator = "multiple"
+    rglob_patterns = _cabc.Sequence[str]
+    object_storage_output_file_path: ObjectStorageOutputZipFilePath
+
+
+type Result = SingleFileResult | MultipleFilesResult
+
+
+class Command(_pyd.BaseModel):
     program: _pcom.PureWindowsPath
     args: _cabc.Sequence[str]
     working_dir: _pcom.PureWindowsPath | None = None
     relative_log_file_path: _pcom.PureWindowsPath | None = None
-    results_glob_pattern: str | None = None
+
+
+class RunnerJob(_pyd.BaseModel):
+    id: str
+    object_storage_input_path: ObjectStorageInputZipFilePath
+    commands: _cabc.Sequence[Command]
+    results: _cabc.Sequence[
+        _tp.Annotated[Result, _pyd.Field(discriminator="discriminator")]
+    ]
+    return_paths_rglob_pattern: str | None = None
