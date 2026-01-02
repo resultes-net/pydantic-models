4d3c1b2c1bb0c5b89e1ae6fbbbc18aa476d1bea0
Damian Birchler <damian.birchler@ost.ch>
2026-01-02 12:19:26 +0100

    Add type for latest login.

--- src/resultes_pydantic_models/server.py
+++ src/resultes_pydantic_models/server.py
@@ -3,9 +3,14 @@ import collections.abc as _cabc
 import pydantic as _pyd
 import resultes_pydantic_models.simulations.simulation as _psim
 import resultes_pydantic_models.simulations.variation as _pvar
+import resultes_pydantic_models.common as _pcom
 
 
 class WaitingVariations(_pyd.BaseModel):
     waiting_variations: _cabc.Sequence[_pvar.Variation]
     associated_simulations: _cabc.Sequence[_psim.Simulation]
     other_variations: _cabc.Sequence[_pvar.Variation]
+
+
+class LatestLogin(_pyd.BaseModel):
+    on: _pcom.AwarePastDatetime | None
