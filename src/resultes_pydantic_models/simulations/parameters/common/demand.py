fc3032e96fdfda74db054183414a562498e2a386
Damian Birchler <damian.birchler@ost.ch>
2026-05-06 09:06:07 +0200

    Make user provided profile not a blob but a list of hourly demand values.

--- src/resultes_pydantic_models/simulations/parameters/common/demand.py
+++ src/resultes_pydantic_models/simulations/parameters/common/demand.py
@@ -1,6 +1,9 @@
 import typing as _tp
+import collections.abc as _cabc
 
 import pydantic as _pc
+
+N_HOURS_PER_YEAR = 365 * 24
 
 
 class PreDefinedProfile(_pc.BaseModel):
@@ -10,7 +13,10 @@ class PreDefinedProfile(_pc.BaseModel):
 
 class UserProvidedProfile(_pc.BaseModel):
     profile_type: _tp.Literal["user-provided"]
-    data: bytes
+    hourly_heat_demand_kW: _tp.Annotated[
+        _cabc.Sequence[float],
+        _pc.Field(min_length=N_HOURS_PER_YEAR, max_length=N_HOURS_PER_YEAR),
+    ]
 
 
 class Demand(_pc.BaseModel):
