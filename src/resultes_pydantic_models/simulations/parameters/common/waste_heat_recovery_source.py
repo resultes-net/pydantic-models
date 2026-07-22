48273be6707883f60e93ff07e6b9f725e8f219ec
Damian Birchler <damian.birchler@ost.ch>
2026-07-22 09:03:57 +0200

    Improve WHR source.

--- src/resultes_pydantic_models/simulations/parameters/common/waste_heat_recovery_source.py
+++ src/resultes_pydantic_models/simulations/parameters/common/waste_heat_recovery_source.py
@@ -2,20 +2,22 @@ import collections.abc as _cabc
 import typing as _tp
 
 import pydantic as _pc
-import resultes_pydantic_models.simulations.parameters.common.profiles as _pprofs
+from resultes_pydantic_models.simulations.parameters.common.profiles import (
+    N_HOURS_PER_YEAR,
+)
 
 
 class MassFlowRateAndTemperature(_pc.BaseModel):
-    mass_flow_rate_kg_per_h: float
-    temperature_deg_C: float
+    mass_flow_rate_kg_per_h: _pc.NonNegativeFloat
+    temperature_deg_C: _tp.Annotated[float, _pc.Field(ge=0, le=0)]
 
 
 class WasteHeatRecoverySource(_pc.BaseModel):
     name: str
-    values: _tp.Annotated[
+    hourly_values: _tp.Annotated[
         _cabc.Sequence[MassFlowRateAndTemperature],
         _pc.Field(
-            min_length=_pprofs.N_HOURS_PER_YEAR,
-            max_length=_pprofs.N_HOURS_PER_YEAR,
+            min_length=N_HOURS_PER_YEAR,
+            max_length=N_HOURS_PER_YEAR,
         ),
     ]
