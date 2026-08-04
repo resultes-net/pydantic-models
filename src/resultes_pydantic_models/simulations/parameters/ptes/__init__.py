72274a6007ebb6267b3a1a502834093c561efcf1
Damian Birchler <damian.birchler@ost.ch>
2026-08-04 23:03:24 +0200

    Extract common parameters.

--- src/resultes_pydantic_models/simulations/parameters/ptes/__init__.py
+++ src/resultes_pydantic_models/simulations/parameters/ptes/__init__.py
@@ -1,20 +1,9 @@
 import typing as _tp
 
-import pydantic as _pc
-
-from ..common import collector_field as _cf
-from ..common import control as _ctrl
-from ..common import demand as _demand
-from ..common import time as _time
-from ..common import waste_heat_recovery_source as _whrs
+from ..common import parameters_base as _pb
 from .parameters import thermal_energy_storage as _tes
 
 
-class PtesParameters(_pc.BaseModel):
+class PtesParameters(_pb.ParametersBase):
     type: _tp.Literal["ptes"]
-    time: _time.Time
-    demand: _demand.Demand
-    collector_field: _cf.CollectorField
-    waste_heat_recovery_source: _whrs.WasteHeatRecoverySource
     storage: _tes.PtesStorage
-    control: _ctrl.Control
--- src/resultes_pydantic_models/simulations/parameters/ttes/__init__.py
+++ src/resultes_pydantic_models/simulations/parameters/ttes/__init__.py
@@ -1,20 +1,9 @@
 import typing as _tp
 
-import pydantic as _pc
-
-from ..common import collector_field as _cf
-from ..common import control as _ctrl
-from ..common import demand as _demand
-from ..common import time as _time
-from ..common import waste_heat_recovery_source as _whrs
+from ..common import parameters_base as _pb
 from .parameters import thermal_energy_storage as _tes
 
 
-class TtesParameters(_pc.BaseModel):
+class TtesParameters(_pb.ParametersBase):
     type: _tp.Literal["ttes"]
-    time: _time.Time
-    demand: _demand.Demand
-    collector_field: _cf.CollectorField
-    waste_heat_recovery_source: _whrs.WasteHeatRecoverySource
     storage: _tes.TtesStorage
-    control: _ctrl.Control
