de1e13f85e400ed5dd2e89a9acccb1aae3025839
Damian Birchler <damian.birchler@ost.ch>
2026-09-04 11:08:36 +0200

    [BTES] Add parameters for borehole HXs.

--- src/resultes_pydantic_models/simulations/parameters/btes/parameters/thermal_energy_storage.py
+++ src/resultes_pydantic_models/simulations/parameters/btes/parameters/thermal_energy_storage.py
@@ -4,6 +4,11 @@ import pydantic as _pc
 import resultes_pydantic_models.simulations.parameters.common.scaled_value as _common
 
 
+class HeatExchanger(_pc.BaseModel):
+    fluid_to_ground_resistance_m_K_per_W: float
+    pipe_to_pipe_resistance_m_K_per_W: float
+
+
 class BtesStorage(_pc.BaseModel):
     n_boreholes: _common.ScaledValue[
         _tp.Literal[
@@ -14,3 +19,4 @@ class BtesStorage(_pc.BaseModel):
     ]
     borehole_spacing_m: float
     borehole_depth_m: float
+    heat_exchanger: HeatExchanger
